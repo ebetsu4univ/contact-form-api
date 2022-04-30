bf461ca039509a111b042f2917d933182cfe00ea
Yuuto Harada <mryuu.dev@gmail.com>
2022-04-30 13:30:04 +0900

    feat: Change the contents of emails sent to users

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ```shell
 echo "export SENDGRID_API_KEY='YOUR_API_KEY'" > sendgrid.env
 source ./sendgrid.env
+vi ./sendgrid.env
+# Add this line
+# export DISCORD_INVITATION_URL='<your discord invitation url>'
 
 npm run start
 ```
--- app.ts
+++ app.ts
@@ -29,12 +29,13 @@ app.listen(port, hostName, () => {
 const sendEmail = (recipient: string) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
+  const discordURL = process.env.DISCORD_INVITATION_URL;
   const msg = {
     to: recipient,
     from: 'mryuu.dev@gmail.com',
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    subject: '【4大学コミュニティー】Discordへの招待URLをお送りします',
+    text: '※ このメールはシステムからの自動返信です. こんにちわ! 江別４大学コミュニティ(E4U)です. この度は, 私達に興味を持ってくださりありがとうございます. Discordへの招待URLは下記になります. 招待URL: ' + discordURL + 'では, Discordにてお会いしましょう. この度はご連絡いただき誠にありがとうございました. ※ 添付の招待URLが期限切れ等の理由で無効だった場合は, 大変申し訳ございませんが江別4大学コミュニティー公式Twitterまでご連絡いただければ幸いです.',
+    html: "※ このメールはシステムからの自動返信です.<br><br>こんにちわ! 江別４大学コミュニティ(E4U)です.<br>この度は, 私達に興味を持ってくださりありがとうございます.<br><br>Discordへの招待URLは下記になります.<br>招待URL: " + discordURL  + "<br><br>では, Discordにてお会いしましょう.<br>この度はご連絡いただき誠にありがとうございました.<br><br>※ 添付の招待URLが期限切れ等の理由で無効だった場合は, 大変申し訳ございませんが<a href='https://twitter.com/Ebetsu_4U'>江別4大学コミュニティー公式Twitter</a>までご連絡いただければ幸いです.",
   };
   sgMail.send(msg)
     .then(() => {
